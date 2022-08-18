177c397c5e7050e58d2b01592d31263d3ab939a3
Adam Murray <a.murray0413@gmail.com>
2022-08-18 08:58:13 -0700

    Randomized Prims Maze

---
+++ mazes/randomized-prims/solution_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 3.10
+
+"""Create your maze solver here."""
+
+import matplotlib.pyplot as plt
+
+import randomized_prims_maze_generator as prims
+
+HEIGHT = 100
+WIDTH = 100
+
+maze = prims.maze_gen(HEIGHT, WIDTH)
+
+START = (0, 1)
+END = (-1, -2)
+
+maze[START] = 0.4
+maze[END] = 0.6
+
+print(maze)
+plt.imshow(maze, cmap='gnuplot2')
+plt.show()
