ff2afc1851f317f5904ff5d111ced504e97084b4
Adam Murray <a.murray0413@gmail.com>
2022-08-18 09:43:36 -0700

    Randomized Prims Maze

--- mazes/randomized-prims/adiaslows_original_solution.py
+++ mazes/randomized-prims/adiaslows_original_solution.py
@@ -3,11 +3,8 @@
 """Create your maze solver here."""
 
 import matplotlib.pyplot as plt
-import time
-
+import numpy as np
 import randomized_prims_maze_generator as prims
-
-start_time = time.time()
 
 HEIGHT = 100
 WIDTH = 100
@@ -19,11 +16,6 @@ END = (-1, -2)
 
 maze[START] = 0.4
 maze[END] = 0.6
-
-
-end_time = time.time()
-print("Run Time = " + str(end_time-start_time))
-print()
 
 print(maze)
 plt.imshow(maze, cmap='gnuplot2')
